169d53d4b5f1f4b7fe1177b3c3d18149d833f782
PORTAKAL MEDYA <you@example.com>
2017-11-21 17:37:29 +0300

    dddddddddd

---
+++ app/TercumanIsTakip.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TercumanIsTakip extends Model
+{
+    //
+}
---
+++ database/migrations/2017_11_21_141927_create_tercuman_is_takip_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTercumanIslersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tercuman_is_takip', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('TercumanID');
+            $table->integer('BirimFiyat');
+            $table->date('EvrakAlmaTarihi');
+            $table->string('ProjeAdi');
+            $table->integer('KarakterSayisi');
+            $table->string('KaynakDil');
+            $table->string('HedefDil');
+            $table->string('SubeID');
+            $table->string('TemsilciID');
+            $table->string('ProjeNot');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tercuman_is_takip');
+    }
+}
